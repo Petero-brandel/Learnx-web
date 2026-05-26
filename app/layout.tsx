e1c6f3dd04a0587d76f84034c9b5fa6857b82385
petero-brandel <peter01rebrandel@gmail.com>
2026-05-27 00:20:16 +0100

    fix: favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -91,14 +91,6 @@ export default function RootLayout({
   suppressHydrationWarning
  >
  <head>
-  <link rel="icon" href="/favicon.ico?v=2" />
-  <link rel="icon" href="/android-chrome-512x512.png?v=3" />
-  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=2" />
-  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=2" />
-  <link rel="apple-touch-icon" href="/apple-touch-icon.png?v=2" />
-  <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png?v=2" />
-  <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png?v=2" />
-  <link rel="manifest" href="/site.webmanifest?v=2" />
   <meta name="theme-color" content="#ffffff" />
   <script
     type="application/ld+json"
